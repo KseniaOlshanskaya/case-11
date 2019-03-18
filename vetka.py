863eb4a42158f576138e13d8b2a1582a481dd4a5
XeniaOlshanskaya <ks14012000@gmail.com>
2019-03-18 17:09:17 +0700

    vetka

---
+++ vetka.py
@@ -0,0 +1,35 @@
+import turtle
+
+
+def branch(n, size):
+    if n == 0:
+        turtle.left(180)
+        return
+
+    x = size/(n+1)
+    for i in range(n):
+        turtle.forward(x)
+        turtle.left(45)
+        branch(n-i-1, 0.5*x*(n-i-1))
+        turtle.left(90)
+        branch(n-i-1, 0.5*x*(n-i-1))
+        turtle.right(135)
+
+    turtle.forward(x)
+    turtle.left(180)
+    turtle.forward(size)
+
+
+def main():
+    h = int(input("Введите длину: "))
+    a = int(input("Введите глубину рекурсии: "))
+    turtle.up()
+    turtle.goto(0, -100)
+    turtle.left(90)
+    turtle.down()
+    branch(a, h)
+    turtle.mainloop()
+
+
+if __name__ == "__main__":
+    main()
